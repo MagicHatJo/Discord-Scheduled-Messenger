7cfd079cb2affe02e0024835ef9244f3e0e4a6fa
Jo <42.jochang@gmail.com>
2022-11-30 15:22:36 -0800

    added embed capabilities to discord_hook

--- discord_hook.py
+++ discord_hook.py
@@ -21,34 +21,43 @@ class ReminderBot(discord.Client):
 		print(f'Logged in as {self.user} (ID: {self.user.id})')
 		print('------')
 
-	async def send_message(self, recipient, message="", channel=None):
+	async def send_message(self, recipient, message="", image_url=None, channel=None):
 		"""
 		Sends a given message to the recipient through a mention in the given channel.
 		If no channel is given, recipient recieves the message through a dm.
 
 		:param recipient: ID of recipient.
 		:param message  : Message to be sent.
+		:param image_url: Image to embed with message.
 		:param channel  : Channel to mention recipient.
 		:return: Response of message delivery. 
 		"""
 		await self.wait_until_ready()
 		user = await client.fetch_user(recipient)
+		package = ""
+
+		print(f"Sending |{message}| to {user.name}")
 		if channel is not None:
-			print(f"Found Channel {channel}")
-			channel = self.get_channel(channel)
-			print(f"Sending |{message}| to {user.name} in {channel}")
-			await channel.send(f"{user.mention} {message}")
+			endpoint = self.get_channel(channel)
+			package = f"{user.mention} "
 		else:
-			print(f"Sending |{message}| to {user.name}")
-			await user.send(message)
+			endpoint = user
+		
+		if image_url is not None:
+			embed = discord.Embed(color = 0x303136, description = message)
+			embed.set_image(url = image_url)
+			await endpoint.send(package, embed = embed)
+		else:
+			package += message
+			await endpoint.send(package)
 	
-	async def send_message_on_repeat(self, recipient, message, interval, channel):
+	async def send_message_on_repeat(self, recipient, message, image_url, interval, channel):
 		"""
 		Given an interval in seconds, automatically loops message delivery.
 		"""
 		await self.wait_until_ready()
 		while not self.is_closed():
-			await self.send_message(recipient, message)
+			await self.send_message(recipient, message, image_url, channel)
 			await asyncio.sleep(interval)  # interval in seconds
 
 if __name__ == "__main__":
